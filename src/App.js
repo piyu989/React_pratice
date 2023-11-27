6866e0cecaebae3e6fd5438157ccc6a320813787
piyush <sab9861@gmail.com>
2023-11-27 15:17:07 +0530

    image adding in slider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import {Carousel} from "./Carousel"
+// import { useState } from "react";
+import img1 from "./images/first.jpeg"
+import img2 from "./images/second.jpeg"
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 function App() {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>sita ram</h1>
+      <Carousel responsive={responsive}>
+      <div className='card'>
+        <img src='./images/first.png' alt='lehanga'/>
+      </div>
+      <div className='card'>
+        <img src='./images/first.png' alt='lehanga'/>
+      </div>
+      <div className='card'>
+        <img src='./images/first.png' alt='lehanga'/>
+      </div>
+      <div className='card'>
+        <img src='./images/first.png' alt='lehanga'/>
+      </div>
+      <div className='card'>
+        <img src='./images/first.png' alt='lehanga'/>
+      </div>
+      <div className='card'>
+        <img src='./images/first.png' alt='lehanga'/>
+      </div>
+      <div className='card'>
+        <img src='./images/first.png' alt='lehanga'/>
+      </div>
+    </Carousel>
     </div>
   );
 }
